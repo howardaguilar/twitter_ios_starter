fb88d114d0b102e8f2aeb3f45e63b1b29ca7101c
Howard Aguilar <haguilar@csumb.edu>
2020-02-11 15:58:28 -0800

    First functionality to login button added

--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -18,7 +18,13 @@ class LoginViewController: UIViewController {
     
     
     @IBAction func onLoginButton(_ sender: Any) {
-        print("Hello World!")
+        let myUrl = "https://api.twitter.com/oauth/request_token"
+        
+        TwitterAPICaller.client?.login(url: myUrl, success: {
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }, failure: { (Error) in
+            print("Could not log in!")
+        })
     }
     
 
